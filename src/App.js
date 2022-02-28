907d7ea4c11c05c42c46bc4fc102dfd9e0aa8bd1
Mosés Ortíz Gracia <moisesisaias23mar@gmail.com>
2022-02-28 18:06:28 -0500

    colores-texto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import QuoteBox from "./components/QuoteBox";
+import Button from "./components/Button";
+import quotes from "./quotes.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const colors = ["aqua", "#b4baf3", "goldenrod", "mediumspringgreen", "#e37c56"];
+    const [color, setColor] = useState(0);
+
+    const [phraseAndAuthor, setPhraseAndAuthor] = useState(0);
+    return (
+        <div className="App">
+            <header className="App-header" style={{ backgroundColor: colors[color] }}>
+                <div className="card">
+                    <QuoteBox colorGlobal={colors[color]} phraseAndAuthor={phraseAndAuthor} />
+                    <Button
+                        arrayColors={colors}
+                        color={color}
+                        setColor={setColor}
+                        arrayQuotes={quotes}
+                        phraseAndAuthor={phraseAndAuthor}
+                        setPhraseAndAuthor={setPhraseAndAuthor}
+                    />
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
